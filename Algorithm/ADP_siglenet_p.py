74ab90d415793f6550ae5d674c184e42dfabe162
ht <2402395996@qq.com>
2022-09-26 20:37:06 +0800

    first commit
    Signed-off-by: ht <2402395996@qq.com>

--- Algorithm/ADP_siglenet_p.py
+++ Algorithm/ADP_siglenet_p.py
@@ -25,6 +25,7 @@ Created on Wed Jan 17 10:40:44 2018
 
 import numpy as np
 import torch as t
+from torch.autograd import Variable
 
 """
 ********************************************************************************************************
@@ -63,16 +64,15 @@ Fre_V1_paras = 5
 
 
 ############################################################################################################
-# 定义网络
+# Define Network
 ############################################################################################################
-
 
 class Model(t.nn.Module):
     def __init__(self, input_dim, output_dim):
         super(Model, self).__init__()
         self.lay1 = t.nn.Linear(input_dim, 10, bias=False)
         self.lay1.weight.data.normal_(0, 0.5)
-        self.lay2 = t.nn.Linear(10, output_dim, bais=False)
+        self.lay2 = t.nn.Linear(10, output_dim, bias=False)
         self.lay2.weight.data.normal_(0, 0.5)
 
     def forward(self, x):
@@ -82,13 +82,19 @@ class Model(t.nn.Module):
         return output
 
 
-class ADP_s():
-    def __init__(self, evn):
+############################################################################################################
+# the policy based ADP with single net
+############################################################################################################
+
+class ADPSingleNet(object):
+    def __init__(self, evn, replay_buffer):
         """
 
+        :param evn:
         """
         'init evn'
         self.evn = evn()
+        self.buffer = replay_buffer
 
         self.state_dim = evn.state_dim
         self.state = np.zeros(state_dim)
@@ -99,36 +105,44 @@ class ADP_s():
         'reward parameter'
         self.Q = t.tensor(evn.Q)
         self.R = t.tensor(evn.R)
+        self.gamma = 1
 
         'init critic net'
         self.critic_eval = Model(input_dim=self.state_dim, output_dim=1)
         self.critic_target = Model(input_dim=self.state_dim, output_dim=1)
-        self.Criterion = t.nn.MSELoss(reduction='mean')
-        self.optimizerV2 = t.optim.SGD(self.critic_eval.parameters(), lr=learning_rate)
+        self.criterion = t.nn.MSELoss(reduction='mean')
+        self.optimizerCritic = t.optim.SGD(self.critic_eval.parameters(), lr=learning_rate)
+        self.batch = 32
+        self.update_targetNet_freq = 10
 
     def choose_action(self, state):
         """
-
+        :brief:
+        :param state:  the current state
+        :return:       the optimal action
         """
         "Step one: calculate the gradient of the critic net"
-        state = t.tensor(state, requires_grad=True)
+        state = t.tensor(state, dtype=t.float, requires_grad=True)
         critic_value = self.critic_eval(state)
         critic_value.backward()
         critic_grad = state.grad
         "Step one: calculate the gradient of the critic net"
 
         "Step two: calculate the action according to the HJB function and system dynamic function"
-        value = t.mm(self.R, self.gx)
-        action = t.mm(va)
+        value = t.mm(t.pinverse(self.R), self.gx)
+        action = - t.mm(value, critic_grad.T) / 2
         "Step two: calculate the action according to the HJB function and system dynamic function"
+        return action
 
-    def learn(self, replay_buffer):
+    def learn(self):
         """
 
+        :return:
         """
         for train_index in range(learning_num):
+
             "Step one; get data"
-            state, action, reward, state_new, done = self.memory_buffer()
+            state, action, reward, state_new, done = self.buffer.buffer_sample_batch(batch_size=self.batch)
             state = t.tensor(state, dtype=t.float)
             action = t.tensor(action, dtype=t.float)
             reward = t.tensor(reward, dtype=t.float)
@@ -136,5 +150,28 @@ class ADP_s():
             "Step one; get data"
 
             "Step two: calculate critic value"
-
+            critic_value = self.critic_eval(Variable(state))
+            critic_value_next = self.critic_target(Variable(state_new))
             "Step two: calculate critic value"
+
+            "calculate the loss and update critic net"
+            critic_loss = self.criterion(critic_value, Variable(reward + self.gamma * critic_value_next))
+            self.optimizerCritic.zero_grad()
+            critic_loss.backward()
+            self.optimizerCritic.step()
+            print('_______the Critic Net have updated for %d time_______' % train_index)
+            "calculate the loss and update critic net"
+
+            "update parameters of critic target net"
+            if train_index % self.update_targetNet_freq == 0:
+                pass
+            "update parameters of critic target net"
+
+    def save_models(self):
+        pass
+
+    def load_models(self):
+        pass
+
+    def update_network_parameters(self):
+        pass
